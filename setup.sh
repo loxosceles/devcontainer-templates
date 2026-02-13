aca12549f5b488490d9171f7d569beec4c47af20
Magnus Henkel <dev@loxosceles.me>
2026-02-13 17:11:08 +0000

    fix: Replace slashes with dashes in archive prefix

--- setup.sh
+++ setup.sh
@@ -205,7 +205,8 @@ cd .devcontainer
 
 # Determine branch/ref to use
 BRANCH="${DEVCONTAINER_TEMPLATES_BRANCH:-main}"
-ARCHIVE_PREFIX="devcontainer-templates-${BRANCH}"
+# GitHub replaces slashes with dashes in archive names
+ARCHIVE_PREFIX="devcontainer-templates-${BRANCH//\//-}"
 
 # Download template
 print_info "Downloading $TEMPLATE template..."
